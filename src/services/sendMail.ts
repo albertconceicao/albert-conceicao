00c0a7fe6de16bed5071b50750e3a6ced2657d55
Albert Conceição <developer.albert@outlook.com>
2023-03-23 13:51:03 -0300

    uploading again on netlify

--- src/services/sendMail.ts
+++ src/services/sendMail.ts
@@ -8,7 +8,10 @@ export const sendContactMail = async (name, senderMail, content) => {
   };
 
   try {
-    const res = await axios.post('/api/contact', data);
+    const res = await axios.post(
+      'https://ubiquitous-bienenstitch-b45864/api/contact',
+      data
+    );
     return res;
   } catch (error) {
     console.error(error);
