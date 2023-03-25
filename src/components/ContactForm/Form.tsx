ac0d72693423fcc237eb3d6ff89a999b6b0b336a
Albert Conceição <developer.albert@outlook.com>
2023-03-24 23:03:32 -0300

    testing formsubmit.co

--- src/components/ContactForm/Form.tsx
+++ src/components/ContactForm/Form.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import { toast } from 'react-hot-toast';
-import { sendContactMail } from '../../services/sendMail';
-import theme from '../../styles/theme';
+// import { toast } from 'react-hot-toast';
+// import { sendContactMail } from '../../services/sendMail';
+// import theme from '../../styles/theme';
 import { FormContainer, Input, TextArea } from './styles';
 
 export function Form() {
@@ -9,43 +9,49 @@ export function Form() {
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
 
-  const handleSubmit = async event => {
-    event.preventDefault();
-    if (!name || !email.includes('@') || !message) {
-      toast('Preencha todos os campos para enviar sua mensagem', {
-        style: {
-          background: theme.error,
-          color: '#fff'
-        }
-      });
-      return;
-    }
-    try {
-      await sendContactMail(name, email, message);
-      setName('');
-      setEmail('');
-      setMessage('');
-      toast('Email enviado com sucesso', {
-        style: {
-          background: theme.secondary,
-          color: '#fff'
-        }
-      });
-    } catch (error) {
-      toast('Ocorreu um erro ao tentar enviar sua mensagem. Tente novamente!', {
-        style: {
-          background: theme.error,
-          color: '#fff'
-        }
-      });
-    }
-  };
+  // const handleSubmit = async event => {
+  //   event.preventDefault();
+  //   if (!name || !email.includes('@') || !message) {
+  //     toast('Preencha todos os campos para enviar sua mensagem', {
+  //       style: {
+  //         background: theme.error,
+  //         color: '#fff'
+  //       }
+  //     });
+  //     return;
+  //   }
+  //   try {
+  //     await sendContactMail(name, email, message);
+  //     setName('');
+  //     setEmail('');
+  //     setMessage('');
+  //     toast('Email enviado com sucesso', {
+  //       style: {
+  //         background: theme.secondary,
+  //         color: '#fff'
+  //       }
+  //     });
+  //   } catch (error) {
+  //     toast('Ocorreu um erro ao tentar enviar sua mensagem. Tente novamente!', {
+  //       style: {
+  //         background: theme.error,
+  //         color: '#fff'
+  //       }
+  //     });
+  //   }
+  // };
   return (
-    <FormContainer data-aos="fade-up" onSubmit={handleSubmit}>
+    <FormContainer
+      data-aos="fade-up"
+      // onSubmit={handleSubmit}
+      action="https://formsubmit.co/developer.albert@outlook.com"
+      method="POST"
+    >
       <Input
         placeholder="Nome"
         type="text"
         onChange={({ target }) => setName(target.value)}
+        name="name"
         value={name}
       />
       <Input
@@ -53,11 +59,13 @@ export function Form() {
         type="email"
         onChange={({ target }) => setEmail(target.value)}
         value={email}
+        name="email"
       />
       <TextArea
         placeholder="Mensagem"
         onChange={({ target }) => setMessage(target.value)}
         value={message}
+        name="message"
       />
       <button type="submit">ENVIAR</button>
     </FormContainer>
