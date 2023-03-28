79d7eec8408893d7e0e8b7bd46a3f0b48e0d6b20
Albert Conceição <developer.albert@outlook.com>
2023-03-28 09:35:30 -0300

    feat: sending form to email and Google Sheet

--- src/components/ContactForm/Form.tsx
+++ src/components/ContactForm/Form.tsx
@@ -8,6 +8,27 @@ export function Form() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState('');
+  const date = new Date();
+  const actualDate = date.toLocaleDateString();
+  const actualHour = date.toLocaleTimeString();
+  const dataApi = {
+    Nome: name,
+    Email: email,
+    Mensagem: message,
+    _subject: 'Contato via Portfólio',
+    _template: 'box',
+    Data_E_Hora: `${actualDate} - ${actualHour}`
+  };
+
+  const urlsApi = [
+    'https://formsubmit.co/ajax/developer.albert@outlook.com',
+    'https://api.sheetmonkey.io/form/iVRPQew4afhjTa4AVx7BhG'
+  ];
+
+  const sendDataToAPIs = async (data: any, urls: any) => {
+    const promises = urls.map(url => axios.post(url, data));
+    await Promise.all(promises);
+  };
 
   const handleSubmit = async (event: any) => {
     event.preventDefault();
@@ -21,14 +42,7 @@ export function Form() {
       return;
     }
     axios.defaults.headers.post['Content-Type'] = 'application/json';
-    await axios
-      .post('https://formsubmit.co/ajax/developer.albert@outlook.com', {
-        Nome: name,
-        Email: email,
-        Mensagem: message,
-        _subject: 'Contato via portfólio',
-        _template: 'box'
-      })
+    sendDataToAPIs(dataApi, urlsApi)
       .then(response => {
         setName('');
         setEmail('');
@@ -54,6 +68,39 @@ export function Form() {
         );
         return error;
       });
+    // await axios
+    //   .post('https://formsubmit.co/ajax/developer.albert@outlook.com', {
+    //     Nome: name,
+    //     Email: email,
+    //     Mensagem: message,
+    //     _subject: 'Contato via portfólio',
+    //     _template: 'box'
+    //   })
+    //   .then(response => {
+    //     setName('');
+    //     setEmail('');
+    //     setMessage('');
+    //     toast('Email enviado com sucesso', {
+    //       style: {
+    //         background: theme.secondary,
+    //         color: '#fff'
+    //       }
+    //     });
+    //     return response;
+    //   })
+    //   .catch(error => {
+    //     console.log(error);
+    //     toast(
+    //       'Ocorreu um erro ao tentar enviar sua mensagem. Tente novamente!',
+    //       {
+    //         style: {
+    //           background: theme.error,
+    //           color: '#fff'
+    //         }
+    //       }
+    //     );
+    //     return error;
+    //   });
   };
 
   return (
