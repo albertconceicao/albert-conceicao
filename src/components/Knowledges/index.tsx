2f4723eba38d8263a4c4a5828ca90b81fd1d2818
Albert Conceição <developer.albert@outlook.com>
2023-03-27 23:42:19 -0300

    adding knowledges on section

--- src/components/Knowledges/index.tsx
+++ src/components/Knowledges/index.tsx
@@ -2,7 +2,8 @@ import { AiFillHtml5 } from 'react-icons/ai';
 import { FaCss3Alt, FaReact, FaNodeJs } from 'react-icons/fa';
 import { IoLogoJavascript } from 'react-icons/io5';
 import { TbBrandNextjs } from 'react-icons/tb';
-import { SiTypescript } from 'react-icons/si';
+import { SiTypescript, SiMysql, SiSass, SiJquery } from 'react-icons/si';
+import { BsGit } from 'react-icons/bs';
 import { Container } from './styles';
 import { SectionTitle } from '../SectionTitle';
 import { KnowledgeItem } from './KnowledgeItem';
@@ -16,9 +17,13 @@ export function Knowledges() {
         <KnowledgeItem title="CSS" icon={<FaCss3Alt />} />
         <KnowledgeItem title="JS" icon={<IoLogoJavascript />} />
         <KnowledgeItem title="TS" icon={<SiTypescript />} />
+        <KnowledgeItem title="JQuery" icon={<SiJquery />} />
         <KnowledgeItem title="React" icon={<FaReact />} />
         <KnowledgeItem title="Next.js" icon={<TbBrandNextjs />} />
         <KnowledgeItem title="Node.js" icon={<FaNodeJs />} />
+        <KnowledgeItem title="MySQL" icon={<SiMysql />} />
+        <KnowledgeItem title="Git" icon={<BsGit />} />
+        <KnowledgeItem title="SASS" icon={<SiSass />} />
       </section>
     </Container>
   );
